b6172259cab63a62eb3e86ed077958f7f5eee83d
Matt Frey <matt6frey@gmail.com>
2018-05-17 18:13:19 +0000

    Adding register and login features. Updated 'username' to 'user_id'

--- express-server.js
+++ express-server.js
@@ -1,9 +1,9 @@
 'use-strict';
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+let express = require('express');
+let app = express();
+let PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser');
+let cookieParser = require('cookie-parser');
 
 function generateRandomString() {
   let short = '';
@@ -35,6 +35,20 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//User DB
+const users = {
+  "x7hB4n": {
+    id: "matt6frey",
+    email: "matt6frey@gmail.com",
+    password: "mf6Doom"
+  },
+  "2BBsl2": {
+    id: "sukiYoJimbo",
+    email: "suki@gmail.com",
+    password: "businessTime7"
+  }
+};
+
 //Root
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -43,13 +57,13 @@ app.get("/", (req, res) => {
 //Route Redirect for Short URLS
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  res.redirect( longURL);
 });
 
 //Route for link list
 app.get("/urls", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    user_id: req.cookies["user_id"],
     urls: urlDatabase
   };
   res.render("urls_index", templateVars);
@@ -59,21 +73,55 @@ app.get("/urls", (req, res) => {
 app.post("/urls", (req, res) => {
   let short = generateRandomString();
   urlDatabase[short] = req.body.longURL;
-  res.redirect(`/urls/${ short }`);
+  res.redirect( `/urls/${ short }`);
 });
 
-//Route for Form
+//Route for New URL Form
 app.get("/urls/new", (req, res) => {
-  var  templateVars = {
-    username: req.cookies["username"]
-  }
+  let  templateVars = {
+    user_id: req.cookies["user_id"]
+  };
   res.render("urls_new", templateVars);
+});
+
+//Route for Register Form
+app.get("/urls/register", (req, res) => {
+  let  templateVars = {
+    user_id: req.cookies["user_id"]
+  };
+  res.render("urls_register", templateVars);
+});
+
+app.post("/urls/register", (req, res) => {
+  newUserID = generateRandomString();
+  var userList = Object.keys(users);
+  userList.forEach((user) => {
+    console.log(user);
+    if (users[user].email === req.body.email) {
+      res.send(res.sendStatus(404));
+    }
+  });
+  if (req.body.email === '' || req.body.email === undefined || req.body.password === '' || req.body.password === undefined) {
+    res.sendStatus(404);
+  } else {
+    users[newUserID] = {
+      id: newUserID,
+      email: req.body.email,
+      password: req.body.password
+    };
+    res.cookie("user_id", newUserID, { expires: new Date(Date.now() + (60*60*24)) });
+    templateVars = {
+      user_id: req.cookies["user_id"],
+      urls: urlDatabase
+    };
+    res.redirect('/urls/');
+  }
 });
 
 //Route for short links
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    user_id: req.cookies["user_id"],
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id]
     };
@@ -84,7 +132,7 @@ app.get("/urls/:id", (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
   res.statusCode = 200;
   delete urlDatabase[req.params.id];
-  res.redirect('/urls/');
+  res.redirect( '/urls/');
 });
 
 //Route for editing URLs
@@ -92,23 +140,22 @@ app.post("/urls/:id/edit", (req, res) => {
   res.statusCode = 200;
   // console.log();
   urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect(`/urls/${ req.params.id }/`);
+  res.redirect( `/urls/${ req.params.id }/`);
 });
 
 //Route for logging in.
 app.post('/login', (req, res) => {
   // Set Username value in cookie.
-  res.cookie("username", req.body.username, { expires: new Date(Date.now() + (60*60*24)) });
-  //console.log("Username entered: ", req.body.username);
+  res.cookie("user_id", req.body.username, { expires: new Date(Date.now() + (60*60*24)) });
   res.redirect("/urls/");
 });
 
 //Route for logging out.
 app.post('/logout', (req, res) => {
   // Set Username value in cookie.
-  res.clearCookie("username", req.body.username, { expires: -999 });
+  res.clearCookie("user_id", req.body.username, { expires: - 999 });
   //console.log("Username entered: ", req.body.username);
-  res.redirect("/urls/");
+  res.redirect( "/urls/");
 });
 
 //Route to JSON Data of URLS
